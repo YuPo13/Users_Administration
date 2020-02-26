8be8784ffe58de601b91e692551896f160827c60
Yuliya Povzlo <yuliya.povzlo@gmail.com>
2020-02-26 18:03:02 +0200

    Improvements according to AB review: - formatting changed, - csrf token added

--- users/forms.py
+++ users/forms.py
@@ -13,18 +13,18 @@ class RegistrationForm(UserCreationForm):
         'class': 'form-control',
         'placeholder': 'Enter your surname here'
     }))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={
+    username = forms.EmailField(widget=forms.EmailInput(attrs={
         'class': 'form-control',
         'placeholder': 'example@mail.com'
-        }))
+    }))
     password1 = forms.CharField(label="Password", widget=forms.PasswordInput(attrs={
         'class': 'form-control',
         'placeholder': 'password'
-        }))
+    }))
     password2 = forms.CharField(label="Password", widget=forms.PasswordInput(attrs={
         'class': 'form-control',
         'placeholder': 'the same password once more'
-        }))
+    }))
 
     class Meta:
         model = User
--- users/views.py
+++ users/views.py
@@ -2,20 +2,27 @@ from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib import messages
+from django.db import DatabaseError
 from .models import Users, Roles, Rights
 from .forms import RegistrationForm, LoginForm
 
 
 def user_registration(request):
     user_form = RegistrationForm()
+
     if request.method == 'POST':
         user_form = RegistrationForm(request.POST)
-        if user_form.is_valid():
-            user_form.save()
-            messages.success(request, 'Successful registration')
-            return redirect(reverse('users:login'))
-        else:
+
+        try:
+            if user_form.is_valid():
+                user_form.save()
+                messages.success(request, 'Successful registration')
+
+                return redirect(reverse('users:login'))
+
+        except DatabaseError:
             messages.warning(request, "Your data hasn't been saved. Please try again")
+
     return render(request, 'users/registration.html', {
         "user_form": user_form,
     })
@@ -23,19 +30,26 @@ def user_registration(request):
 
 def user_login(request):
     form = LoginForm()
+
     if request.method == 'POST':
         form = LoginForm(request.POST)
+
         if form.is_valid():
-            username = form.cleaned_data['username']
-            user = authenticate(request, username=username, password=form.cleaned_data['password'])
+            email = form.cleaned_data['email']
+            user = authenticate(request, username=email, password=form.cleaned_data['password'])
+
             if user is not None:
                 login(request, user)
                 messages.success(request, "You're logged in.")
-                return redirect(f"/user/profile/{username}")
+
+                return redirect(f"/user/profile/{email}")
+
             else:
                 messages.error(request, "Invalid username or password")
+
         else:
             messages.error(request, "Invalid input")
+
     return render(request, 'users/login.html', {
         'form': form,
         'title': 'Login page'
