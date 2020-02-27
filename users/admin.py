21d96b85005a14ac28758337c00d81f9ccdf722c
Yuliya Povzlo <yuliya.povzlo@gmail.com>
2020-02-27 19:08:47 +0200

    Superfluous models deleted

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from .models import UserProfile, Role, Right
+from .models import UserProfile
 
 admin.site.register(UserProfile)
-admin.site.register(Role)
-admin.site.register(Right)
--- users/models.py
+++ users/models.py
@@ -1,18 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-MAX_STATUS_LENGTH = 500
-
-
-class Right(models.Model):
-    right = models.CharField(primary_key=True, max_length=MAX_STATUS_LENGTH)
-
-
-class Role(models.Model):
-    role = models.CharField(primary_key=True, max_length=MAX_STATUS_LENGTH)
-    rights = models.ManyToManyField(Right, related_name='roles')
-
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
-    role = models.OneToOneField(Role, on_delete=models.CASCADE, null=True)
